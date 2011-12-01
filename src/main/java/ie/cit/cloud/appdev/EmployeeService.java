fb4611504d17663662829b39d514232ce38af93d
bradlp <paul.bradley@emc.com>
2011-12-01 00:58:36 +0000

    JUnit tests added

--- src/main/java/ie/cit/cloud/appdev/EmployeeService.java
+++ src/main/java/ie/cit/cloud/appdev/EmployeeService.java
@@ -2,56 +2,19 @@ package ie.cit.cloud.appdev;
 
 import ie.cit.cloud.appdev.model.Employee;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-/**
- * EmployeeContoller
- * 
- * @author paul bradley
- */
-@Service
-public class EmployeeService {
+public interface EmployeeService {
 
-	private JDBCEmployeeDataBase repository;
-	
-	@Autowired
-	public EmployeeService(EmployeeDataBase employeDataBase) {
-		this.repository = (JDBCEmployeeDataBase) employeDataBase;
-	}
+	public abstract List<Employee> getAllEmployees();
 
-	public List<Employee> getAllEmployees() {
-		return ( this.repository.getAllEmployees());
-	}
-	
-	public int getEmployeesCount() {
-		int size= this.repository.getAllEmployees().size();
-		return repository.getAllEmployees().size();
-	}
-	
-	public Employee addNewEmployee(String firstname, String lastname, String jobtitle, 
-								String department, int salary) {
-		int nextAvailibleID = this.getNextAvailibleEmployeeId();
-		Employee newEmployee = new Employee(nextAvailibleID, firstname, lastname, jobtitle,
-												department, 001, salary);
-		repository.addEmployee(newEmployee);
-		//SecurityContextHolder.getContext().getAuthentication().
-		return newEmployee;
-	}
-	
+	public abstract int getEmployeesCount();
 
-	
-	private int getNextAvailibleEmployeeId(){
-		return (this.getEmployeesCount()+1);
-	
-	}
-	public Employee getEmployeeByName(String firstname, String lastname) {
-		return ( this.repository.getEmployeeByName(firstname, lastname));
-	}
-	public Employee getEmployeeByID(int employeeID) {
-		return ( this.repository.getEmployeeById(employeeID));
-	}
+	public abstract Employee addNewEmployee(String firstname, String lastname,
+			String jobtitle, String department, int salary);
+
+	public abstract Employee getEmployeeByName(String firstname, String lastname);
+
+	public abstract Employee getEmployeeByID(int employeeID);
+
 }
---
+++ src/main/java/ie/cit/cloud/appdev/EmployeeServiceImpl.java
@@ -0,0 +1,70 @@
+package ie.cit.cloud.appdev;
+
+import ie.cit.cloud.appdev.model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+/**
+ * EmployeeContoller
+ * 
+ * @author paul bradley
+ */
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	JDBCEmployeeDataBase repository;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDataBase employeDataBase) {
+		this.repository = (JDBCEmployeeDataBase) employeDataBase;
+	}
+
+	/* (non-Javadoc)
+	 * @see ie.cit.cloud.appdev.EmployeeService#getAllEmployees()
+	 */
+	public List<Employee> getAllEmployees() {
+		return ( this.repository.getAllEmployees());
+	}
+	
+	/* (non-Javadoc)
+	 * @see ie.cit.cloud.appdev.EmployeeService#getEmployeesCount()
+	 */
+	public int getEmployeesCount() {
+		return repository.getAllEmployees().size();
+	}
+	
+	/* (non-Javadoc)
+	 * @see ie.cit.cloud.appdev.EmployeeService#addNewEmployee(java.lang.String, java.lang.String, java.lang.String, java.lang.String, int)
+	 */
+	public Employee addNewEmployee(String firstname, String lastname, String jobtitle, 
+								String department, int salary) {
+		int nextAvailibleID = this.getNextAvailibleEmployeeId();
+		Employee newEmployee = new Employee(nextAvailibleID, firstname, lastname, jobtitle,
+												department, 001, salary);
+		repository.addEmployee(newEmployee);
+		//SecurityContextHolder.getContext().getAuthentication().
+		return newEmployee;
+	}
+	
+	private int getNextAvailibleEmployeeId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see ie.cit.cloud.appdev.EmployeeService#getEmployeeByName(java.lang.String, java.lang.String)
+	 */
+	public Employee getEmployeeByName(String firstname, String lastname) {
+		return ( this.repository.getEmployeeByName(firstname, lastname));
+	}
+	/* (non-Javadoc)
+	 * @see ie.cit.cloud.appdev.EmployeeService#getEmployeeByID(int)
+	 */
+	public Employee getEmployeeByID(int employeeID) {
+		return ( this.repository.getEmployeeById(employeeID));
+	}
+}
