02a268c04f1070b7ed332922490ca6c7cdcecb3c
bradlp <paul.bradley@emc.com>
2011-12-02 01:30:14 +0000

    more updates

--- src/main/java/ie/cit/cloud/appdev/EmployeeServiceImplTest.java
+++ src/main/java/ie/cit/cloud/appdev/EmployeeServiceImplTest.java
@@ -1,62 +1,82 @@
-/**
- * 
- */
-package ie.cit.cloud.appdev;
+	package ie.cit.cloud.appdev;
+	
+	import static org.junit.Assert.*;
+	
+	import java.util.List;
+	
+	import ie.cit.cloud.appdev.*;
+	import ie.cit.cloud.appdev.model.Employee;
+	
+	import org.junit.After;
+	import org.junit.AfterClass;
+	import org.junit.Before;
+	import org.junit.BeforeClass;
+	import org.junit.Test;
+	import org.mockito.Mockito;
+	
+	/**
+	 * @author paul bradley
+	 *
+	 */
 
-import static org.junit.Assert.*;
-import ie.cit.cloud.appdev.*;
-import ie.cit.cloud.appdev.model.Employee;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.mockito.Mockito;
-/**
- * @author paul bradley
- *
- */
 public class EmployeeServiceImplTest {
-
+	
 	private static final String Paul = null;
 	private static final String Bradley = null;
 	private static final String Manager = null;
 	private static final String Cork = null;
 	private EmployeeServiceImpl testService;
 	private JDBCEmployeeDataBase database;
-	  
-	
-	/**
-	 * @throws java.lang.Exception
-	 */
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
 	@Before
 	public void setUp() throws Exception {
 		database = Mockito.mock(JDBCEmployeeDataBase.class);
 	    testService = new EmployeeServiceImpl(database);
-	    testService.repository=  database;
-	    
+	    //testService.repository= database;
 	}
 
-	/**
-	 * Test method for {@link ie.cit.cloud.appdev.EmployeeServiceImpl#addNewEmployee(java.lang.String, java.lang.String, java.lang.String, java.lang.String, int)}.
-	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testEmployeeServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllEmployees() {
+		List<Employee> employees = testService.getAllEmployees();
+		assertNotNull(employees);
+		assertTrue(employees.isEmpty());
+	}
+
+	@Test
+	public void testGetEmployeesCount() {
+		fail("Not yet implemented");
+	}
+
 	@Test
 	public void testAddNewEmployee() {
-		Employee employee = new Employee(0, Paul, Bradley, Manager, Cork, 0, 0);
-		testService.addNewEmployee(Paul, Bradley, Manager, Cork, 0);
-		//(database).addEmployee(employee);
-		//fail("Not yet implemented");
+		fail("Not yet implemented");
 	}
-	
+
 	@Test
-	public void testgetEmployeesCount() {
-		Employee employee = new Employee(0, Paul, Bradley, Manager, Cork, 0, 0);
-		testService.addNewEmployee(Paul, Bradley, Manager, Cork, 0);
-		int checkCount = testService.getEmployeesCount();
-		checkCount=1;
-		//following prints "element" 
-		System.out.println(testService.getEmployeesCount()); 
-		assertEquals(1,checkCount);
+	public void testGetEmployeeByName() {
+		fail("Not yet implemented");
 	}
+
+	@Test
+	public void testGetEmployeeByID() {
+		fail("Not yet implemented");
+	}
+
 }
