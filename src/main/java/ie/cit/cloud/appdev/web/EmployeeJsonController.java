555034b4d1098e2dd28fa8499706aaf65c0a359a
bradlp <paul.bradley@emc.com>
2011-12-04 23:25:02 +0000

    some POM updates

--- src/main/java/ie/cit/cloud/appdev/web/EmployeeJsonController.java
+++ src/main/java/ie/cit/cloud/appdev/web/EmployeeJsonController.java
@@ -25,16 +25,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /*
- * listAll:   curl http://localhost:8080/week_4_lab_res/employeeDataBase/listAll
- * update: curl -i -H "Content-Type: application/json" -X PUT -d '{"text":"Remember the Milk"}' http://localhost:8080/week_5_lab_res/api/todo/${id}
- * list:   curl http://localhost:8080/week_4_lab_res/api/list
- * delete: curl -X DELETE curl http://localhost:8080/week_5_lab_res/api/todo/${id}
- * detail: curl http://localhost:8080/week_5_lab_res/api/todo/${id}
- * 
- * 
- * Using security:
- * curl -c cookies.txt --data "j_username=igor&j_password=igor" http://localhost:8080/week_10/j_spring_security_check
- * curl -b cookies.txt 
  */
 @Controller
 @RequestMapping("employeeDataBase")
